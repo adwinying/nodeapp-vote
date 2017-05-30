8b80ae19bb91358f6288c4ec2608abc430a3e4ec
Adwin Ying <adwinying@gmail.com>
2017-05-30 19:26:16 +0900

    Optimize angular build for production

--- angular/src/app/components/navbar/navbar.component.ts
+++ angular/src/app/components/navbar/navbar.component.ts
@@ -10,7 +10,7 @@ import { FlashMessagesService } from 'angular2-flash-messages';
 })
 export class NavbarComponent implements OnInit {
   constructor(
-  	private authService: AuthService,
+  	public authService: AuthService,
     private flashMessage: FlashMessagesService,
     private router: Router
 	) { }
